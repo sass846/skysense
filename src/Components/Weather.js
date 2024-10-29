14ac1d4e5e9edae251d578a259c02c1bbcf500e5
sass846 <skiran.self@gmail.com>
2024-10-30 04:12:50 +0530

    change structure

---
+++ src/Components/Weather.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import '../Styles/Weather.css';
+
+export default function WeatherContainer() {
+  <section className="weather-container">
+    
+  </section>
+}
---
+++ src/apiKeys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  key: "72877c2d132b4b31c2b7f56dc8060b39",
+  base: "https://api.openweathermap.org/data/2.5/",
+}
